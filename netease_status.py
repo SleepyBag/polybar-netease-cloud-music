9a147fb4a4d46d9e8b71336f826329852aeaa34e
Xue Qianming <xueqianming200@gmail.com>
2020-01-15 19:11:10 +0800

    Modify: there is no longer listener

--- netease_status.py
+++ netease_status.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-
+import time
 import sys
 import dbus
 import argparse
@@ -20,56 +19,75 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-first = True
-while first or input():
-    first = False
-    # Default parameters
-    output = '{artist} : {song}'
-    trunclen = 15
+# Default parameters
+output = '{artist} : {song}'
+trunclen = 15
 
-    # parameters can be overwritten by args
-    if args.trunclen is not None:
-        trunclen = args.trunclen
-    if args.custom_format is not None:
-        output = args.custom_format
+# parameters can be overwritten by args
+if args.trunclen is not None:
+    trunclen = args.trunclen
+if args.custom_format is not None:
+    output = args.custom_format
 
-    try:
-        session_bus = dbus.SessionBus()
-        spotify_bus = session_bus.get_object(
-            'org.mpris.MediaPlayer2.netease-cloud-music',
-            '/org/mpris/MediaPlayer2'
-        )
+try:
+    session_bus = dbus.SessionBus()
+    cloud_music_bus = session_bus.get_object(
+        'org.mpris.MediaPlayer2.netease-cloud-music',
+        '/org/mpris/MediaPlayer2'
+    )
 
-        spotify_properties = dbus.Interface(
-            spotify_bus,
-            'org.freedesktop.DBus.Properties'
-        )
+    cloud_music_properties = dbus.Interface(
+        cloud_music_bus,
+        'org.freedesktop.DBus.Properties'
+    )
 
-        metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+    metadata = cloud_music_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
 
-        artist = metadata['xesam:artist'][0]
-        if len(artist) > trunclen:
-            artist = artist[0:trunclen]
-            artist += '...' 
-            if ('(' in artist) and (')' not in artist):
-                artist += ')'
-        
-        song = metadata['xesam:title']
+    playback_status = str(cloud_music_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus'))
+    if 'mpris:length' in metadata:
+        length = float(metadata['mpris:length'])
+        position = float(cloud_music_properties.Get('org.mpris.MediaPlayer2.Player', 'Position'))
+    else:
+        length = 1.
+        position = 0.
+    percent = position / length
 
-        if len(song) > trunclen:
-            song = song[0:trunclen]
-            song += '...' 
-            if ('(' in song) and (')' not in song):
-                song += ')'
-        
-        # Python3 uses UTF-8 by default. 
-        if sys.version_info.major == 3:
-            print('%{o#EA2202}%{+o}%{F#EA2202} %{F-}' + output.format(artist=artist, song=song) + '%{o-}')
-        else:
-            print('%{o#EA2202}%{+o}%{F#EA2202} %{F-}' + output.format(artist=artist, song=song).encode('UTF-8') + '%{o-}')
-    except Exception as e:
-        if isinstance(e, dbus.exceptions.DBusException):
-            print('')
-        else:
-            print(e)
+    artist = metadata['xesam:artist'][0]
+    if len(artist) > trunclen:
+        artist = artist[0:trunclen]
+        artist += '...' 
+        if ('(' in artist) and (')' not in artist):
+            artist += ')'
+    
+    song = metadata['xesam:title']
+
+    if len(song) > trunclen:
+        song = song[0:trunclen]
+        song += '...' 
+        if ('(' in song) and (')' not in song):
+            song += ')'
+
+    if sys.version_info.major == 3:
+        output = output.format(artist=artist, song=song)
+    else:
+        output = output.format(artist=artist, song=song).encode('UTF-8')
+    percent = int((len(output) + 1) * percent)
+    output = '%{o#EA2202}%{+o}' + output[: percent] + '%{-o}' + output[percent:]
+    output = '%{F#EA2202} %{F-}' + output
+    output += '  '
+
+    output += '%{A1:dbus-send --print-reply --dest=org.mpris.MediaPlayer2.netease-cloud-music /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous:}    %{A}'
+    if playback_status == 'Playing':
+        output += '%{A1:dbus-send --print-reply --dest=org.mpris.MediaPlayer2.netease-cloud-music /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause:}    %{A}'
+    else:
+        output += '%{A1:dbus-send --print-reply --dest=org.mpris.MediaPlayer2.netease-cloud-music /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause:}    %{A}'
+    output += '%{A1:dbus-send --print-reply --dest=org.mpris.MediaPlayer2.netease-cloud-music /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next:}    %{A}'
+    print(output)
+    #  print('%{o#EA2202}%{+o}%{F#EA2202} %{F-}' + output + '%{o-}')
+
+except Exception as e:
+    if isinstance(e, dbus.exceptions.DBusException):
+        print('%{A1:i3-msg workspace number 7 Netease; netease-cloud-music:}%{F#EA2202} %{F-}打开网易云音乐 %{A}')
+    else:
+        print(e)
 
